e477e2202755b099ff0f5e2aa20c8fe233508177
Radosław Szymkiewicz <szymkiewicz.radoslaw@gmail.com>
2024-12-02 22:06:02 +0100

    Billing or shipping address can be empty

--- src/services/templates/packing-slips/basic/parts/customer-info.ts
+++ src/services/templates/packing-slips/basic/parts/customer-info.ts
@@ -20,34 +20,52 @@ export function generateCustomerInformation(doc, y, order: Order) {
 
   const customerInformationTop = y + 50;
 
-  doc
-    .fontSize(10)
-    .font("Bold")
-    .text(`${t("packing-slip-bill-to", "Bill to")}:`, 50, customerInformationTop, {align: 'left'})
-    .font("Regular")
-    .text(`${order.billing_address.first_name} ${order.billing_address.last_name}`, 50, customerInformationTop + 15, {align: 'left'})
-    .text(`${order.billing_address.city} ${order.billing_address.postal_code}`, 50, customerInformationTop + 30, {align: 'left'})
-    const billAddress = order.billing_address.address_1;
-    const heightOfBillToAddress = doc.heightOfString(billAddress, { width: 150 })
-    doc.text(billAddress, 50, customerInformationTop + 45 , {align: 'left', width: 150 })
-    .moveDown();
-    
-  doc
-    .fontSize(10)
-    .font("Bold")
-    .text(`${t("packing-slip-ship-to", "Ship to")}:`, 50, customerInformationTop, {align: 'right'})
-    .font("Regular")
-    .text(`${order.shipping_address.first_name} ${order.shipping_address.last_name}`, 50, customerInformationTop + 15, {align: 'right'})
-    .text(`${order.shipping_address.city} ${order.shipping_address.postal_code}`, 50, customerInformationTop + 30, {align: 'right'})
-    .moveDown();
-    const shipAddress = order.shipping_address.address_1;
-    const heightOfShipToAddress = doc.heightOfString(shipAddress, { width: 150 })
-    doc.text(shipAddress, 360, customerInformationTop + 45 , {align: 'right', widdth: 150 })
-    .moveDown();
-    
-  if (heightOfShipToAddress > heightOfBillToAddress) {
-    return customerInformationTop + 45 + heightOfShipToAddress;
-  } else {
+  let heightOfBillToAddress: number | undefined;
+
+  if (order.billing_address) {
+    doc
+      .fontSize(10)
+      .font("Bold")
+      .text(`${t("packing-slip-bill-to", "Bill to")}:`, 50, customerInformationTop, {align: 'left'})
+      .font("Regular")
+      .text(`${order.billing_address.first_name} ${order.billing_address.last_name}`, 50, customerInformationTop + 15, {align: 'left'})
+      .text(`${order.billing_address.city} ${order.billing_address.postal_code}`, 50, customerInformationTop + 30, {align: 'left'})
+      const billAddress = order.billing_address.address_1;
+      heightOfBillToAddress = doc.heightOfString(billAddress, { width: 150 })
+      doc.text(billAddress, 50, customerInformationTop + 45 , {align: 'left', width: 150 })
+      .moveDown();
+  }
+
+  let heightOfShipToAddress: number | undefined;
+  
+  if (order.shipping_address) {
+    doc
+      .fontSize(10)
+      .font("Bold")
+      .text(`${t("packing-slip-ship-to", "Ship to")}:`, 50, customerInformationTop, {align: 'right'})
+      .font("Regular")
+      .text(`${order.shipping_address.first_name} ${order.shipping_address.last_name}`, 50, customerInformationTop + 15, {align: 'right'})
+      .text(`${order.shipping_address.city} ${order.shipping_address.postal_code}`, 50, customerInformationTop + 30, {align: 'right'})
+      .moveDown();
+      const shipAddress = order.shipping_address.address_1;
+      heightOfShipToAddress = doc.heightOfString(shipAddress, { width: 150 })
+      doc.text(shipAddress, 360, customerInformationTop + 45 , {align: 'right', widdth: 150 })
+      .moveDown();
+  }
+
+  if (heightOfBillToAddress && heightOfShipToAddress) {
+    if (heightOfShipToAddress > heightOfBillToAddress) {
+      return customerInformationTop + 45 + heightOfShipToAddress;
+    } else {
+      return customerInformationTop + 45 + heightOfBillToAddress;
+    } 
+  }
+  if (heightOfBillToAddress) {
     return customerInformationTop + 45 + heightOfBillToAddress;
   }
+  if (heightOfShipToAddress) {
+    return customerInformationTop + 45 + heightOfShipToAddress;
+  }
+
+  return customerInformationTop;
 }
